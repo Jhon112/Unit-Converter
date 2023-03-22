f0523abc9d8b5b781bfe41a961110ea5b8a024ab
Jhon Arias <jhonarmont@hotmail.com>
2023-03-21 23:31:18 -0500

    Update conversion_spec to get the root path.

--- spec/requests/conversion_spec.rb
+++ spec/requests/conversion_spec.rb
@@ -1,26 +1,27 @@
 require "rails_helper"
+require_relative "../../app/controllers/conversion_controller"
 
 RSpec.describe "Conversions", type: :request do
   it "converts valid input (kg to lb)" do
-    get "/convert", params: { q: "2kg in lb" }
+    get "/", params: { q: "2kg in lb" }
     expect(response).to have_http_status(:success)
-    expect(response.body).to eq("4.40925")
+    expect(response.body).to eq("4.409248840367555")
   end
 
   it "converts valid input (feet to meters)" do
-    get "/convert", params: { q: "20 feet in meters" }
+    get "/", params: { q: "20 feet in meters" }
     expect(response).to have_http_status(:success)
     expect(response.body).to eq("6.096")
   end
 
   it "does not convert different types of unit (kg to meters)" do
-    get "/convert", params: { q: "2 kg in meters" }
+    get "/", params: { q: "2 kg in meters" }
     expect(response).to have_http_status(:bad_request)
     expect(response.body).to eq("Invalid query")
   end
 
   it "handles invalid input" do
-    get "/convert", params: { q: "hello" }
+    get "/", params: { q: "hello" }
     expect(response).to have_http_status(:bad_request)
     expect(response.body).to eq("Invalid query")
   end
